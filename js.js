fe36bd3764b89aa959d6612cb6e18321f2fc6fad
Cagin Agirdag <besteagirdag@gmail.com>
2023-10-30 09:28:04 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,15 @@
+ScrollReveal({ 
+    reset: true,
+    distance: "20px",
+    duration: 1100,
+    delay: 200
+});
+
+ScrollReveal().reveal('.contentsText', { delay: 200 });
+ScrollReveal().reveal('.experience-h2', { delay: 300 });
+ScrollReveal().reveal('.first', { delay: 100, origin: "left"});
+ScrollReveal().reveal('.second', { delay: 150, origin: "left"});
+ScrollReveal().reveal('.third', { delay: 200, origin: "left"});
+ScrollReveal().reveal('.information-mail', { delay: 100, origin: "bottom"});
+ScrollReveal().reveal('.information-link', { delay: 150, origin: "bottom"});
+ScrollReveal().reveal('.information-github', { delay: 200, origin: "bottom"});
